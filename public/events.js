dd86a8c39855197e14f2b8f9ca10121da4f20bce
duy <ddao0892@gmail.com>
2021-02-11 14:49:32 -0700

    comment section

--- public/events.js
+++ public/events.js
@@ -17,28 +17,40 @@ window.addEventListener('DOMContentLoaded', () => {
     newPicButton.addEventListener('click', e =>{
         getCat();
     })
+
+
     const upvote = document.getElementById('upvote');
     const downvote = document.getElementById('downvote');
     const score = document.querySelector('.score');
     upvote.addEventListener('click', () =>{
         fetch('/kitten/upvote', {method:'PATCH'})
-        .then((res) => res.json())
-        .then((res) => {
+        .then(res => res.json())
+        .then(res => {
            score.innerHTML = res.score
         }).catch((err) =>{
             window.alert(err);
         })
     })
+
     downvote.addEventListener('click', () =>{
         fetch('/kitten/downvote', {method:'PATCH'})
-        .then((res) => res.json())
-        .then((res) => {
-           score.innerHTML = res.score
-        }).catch((err) =>{
-            window.alert(err);
-        })
+            .then(res => res.json())
+            .then(res => {
+            score.innerHTML = res.score
+            }).catch(err =>{
+                window.alert(err);
+            })
     })
 
+    //comment section
 
-
+    const commentBtn = document.querySelector('.comment-form');
+    commentBtn.addEventListener('submit', e => {
+        const commentInput = document.getElementById("user-comment");
+        const userComment = commentInput.value;
+        e.preventDefault();
+        fetch('/kitten/comments', {method: "POST", headers: {'Content-Type' : "application/json"}, body: JSON.stringify({comment: userComment}) })
+        .then(res => res.json())
+        .then(res => console.log(res.comments))
+    })
 })
