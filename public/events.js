5a6a18253421a5b2600e962e021c16b3db975a50
mike <mikemihalchik@gmail.com>
2021-02-11 15:30:58 -0500

    added loading bar

--- public/events.js
+++ public/events.js
@@ -1,9 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
     async function getCat () {
+        const loader = document.querySelector(".loader")
+        loader.innerHTML = "Loading..."
         const catJson = await fetch('/kitten/image');
         const json = await catJson.json()
-        console.log(json)
+        const catImg = document.getElementsByClassName('cat-pic')
+        catImg[0].src = json.src;
+        loader.innerHTML = ""
     }
+    const newPicButton = document.getElementById('new-pic');
+    newPicButton.addEventListener('click', e =>{
+        getCat()
 
-    getCat()
+    })
 })
