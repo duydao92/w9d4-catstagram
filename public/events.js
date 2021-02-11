b297b4e36e6dec138660bc13708bc8c246c1a2d3
duy <ddao0892@gmail.com>
2021-02-11 13:55:23 -0700

    error handling

--- public/events.js
+++ public/events.js
@@ -1,16 +1,23 @@
 window.addEventListener('DOMContentLoaded', () => {
     async function getCat () {
-        const loader = document.querySelector(".loader")
-        loader.innerHTML = "Loading..."
+        const loader = document.querySelector(".loader");
+        loader.innerHTML = "Loading...";
         const catJson = await fetch('/kitten/image');
-        const json = await catJson.json()
-        const catImg = document.getElementsByClassName('cat-pic')
-        catImg[0].src = json.src;
-        loader.innerHTML = ""
+        const catObj = await catJson.json();
+        const catImg = document.getElementsByClassName('cat-pic');
+        loader.innerHTML = "";
+        if (catJson.ok) {
+            catImg[0].src = catObj.src;
+        } else {
+            const errorDiv = document.querySelector('.error');
+            errorDiv.innerHTML = catObj.message;
+        }
     }
     const newPicButton = document.getElementById('new-pic');
     newPicButton.addEventListener('click', e =>{
-        getCat()
-
+        getCat();
     })
+
+
+
 })
